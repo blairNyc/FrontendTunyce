37cf602acde77ec1ac56742ac055ed106593b1cf
blairNyc <blair.milimo@tunycemedia.com>
2023-09-08 09:33:18 +0300

    flowbite, home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,8 @@
-import Tunyce_Logo from '../../public/tunyce_logo.svg'
 const ListItem = ({text}:{text:string})=>(<li className='mx-2'>{text}</li>)
 
 function Header() {
     return (
-        <header className="px-4 py-2 w-screen">
+        <header className="px-4 py-8 w-full">
             <ul className='list-none flex items-center'>
                 <ListItem text='Music' />
                 <ListItem text='Podcast'/>
