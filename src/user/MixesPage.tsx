19283e272abeba83a953e0852d8ebbbd32d341dd
felix-ogutu <felixomondi809@gmail.com>
2023-09-08 14:30:36 +0300

    Implementation of the mixes and music page

--- src/user/MixesPage.tsx
+++ src/user/MixesPage.tsx
@@ -1,79 +1,160 @@
+import { BiShuffle } from "react-icons/bi";
+import { BsFillPlayFill } from "react-icons/bs";
 const MixesPage = () => {
   return (
     <>
       <div className="container">
+        {/*Subtitles*/}
         <div className="flex justify-between">
-          <h3 className="">Mixes</h3>
-          <div className="flex">
-            <p>Play</p>
-            <p>Shuffle</p>
+          <div className="text-red-600">
+            <h5 className=" sm:text-base md:text-xl lg:2xl">My Music</h5>
+          </div>
+          <div className="flex gap-x-5 text-red-600">
+            <div className="flex">
+              <button className=" sm:text-sm md:text-md lg:text-xl">
+                <BsFillPlayFill />
+              </button>
+              <button className=" sm:text-sm md:text-md lg:text-xl ml-1">
+                Play
+              </button>
+            </div>
+            <div className="flex mr-2">
+              <button className="sm:text-sm md:text-md lg:text-xl">
+                <BiShuffle />
+              </button>
+              <button className="sm:text-sm md:text-md lg:text-xl ml-1">
+                Shuffle
+              </button>
+            </div>
           </div>
         </div>
+        {/*End of Subtitle*/}
         {/*Main Card*/}
-        <div className="bg-white shadow-md rounded-md w-75 mt-2 h-auto">
-          <h4>11 Mixes</h4>
-          <div className="max-w-md mx-auto mt-2 flex flex-row  ">
+        <div className="ml-4 mr-4 mx-auto mt-4 rounded-md bg-white">
+          <h4 className="font-semibold text-base mb-3 mt-2">11 Mixes</h4>
+          {/*Grid*/}
+          <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4 ml-2">
             {/*Mix*/}
-            <div className="container">
-              <div className="bg-gray-300 w-32 h-32 rounded-md shadow-md"></div>
-              <p>Partyglow</p>
-              <p>Dj Shady</p>
+            <div className="">
+              <div className="bg-gray-300 w-32 h-32  rounded-md shadow-md"></div>
+              <div className="">
+                <p className="text-base font-bold">Afterglow</p>
+                <p className="text-sm">DJ Lyta</p>
+              </div>
             </div>
+            {/*End of Mix*/}
             {/*Mix*/}
-            <div className="container">
-              <div className="bg-gray-300 w-32 h-32 rounded-md shadow-md"></div>
-              <p>Anyone</p>
-              <p>Dj Dree</p>
+            <div className="">
+              <div className="bg-gray-300 w-32 h-32  rounded-md shadow-md"></div>
+              <div className="">
+                <p className="text-base font-bold">Afterglow</p>
+                <p className="text-sm">DJ Lyta</p>
+              </div>
             </div>
+            {/*End of Mix*/}
             {/*Mix*/}
-            <div className="container">
-              <div className="bg-gray-300 w-32 h-32 rounded-md shadow-md"></div>
-              <p>Cry for Me</p>
-              <p>Dj Tryce</p>
+            <div className="">
+              <div className="bg-gray-300 w-32 h-32  rounded-md shadow-md"></div>
+              <div className="">
+                <p className="text-base font-bold">Afterglow</p>
+                <p className="text-sm">DJ Lyta</p>
+              </div>
             </div>
+            {/*End of Mix*/}
+            {/*Mix*/}
+            <div className="">
+              <div className="bg-gray-300 w-32 h-32  rounded-md shadow-md"></div>
+              <div className="">
+                <p className="text-base font-bold">Afterglow</p>
+                <p className="text-sm">DJ Lyta</p>
+              </div>
+            </div>
+            {/*End of Mix*/}
+            {/*Mix*/}
+            <div className="">
+              <div className="bg-gray-300 w-32 h-32  rounded-md shadow-md"></div>
+              <div className="">
+                <p className="text-base font-bold">Afterglow</p>
+                <p className="text-sm">DJ Lyta</p>
+              </div>
+            </div>
+            {/*End of Mix*/}
+            {/*Mix*/}
+            <div className="">
+              <div className="bg-gray-300 w-32 h-32  rounded-md shadow-md"></div>
+              <div className="">
+                <p className="text-base font-bold">Afterglow</p>
+                <p className="text-sm">DJ Lyta</p>
+              </div>
+            </div>
+            {/*End of Mix*/}
+            {/*Mix*/}
+            <div className="">
+              <div className="bg-gray-300 w-32 h-32  rounded-md shadow-md"></div>
+              <div className="">
+                <p className="text-base font-bold">Afterglow</p>
+                <p className="text-sm">DJ Lyta</p>
+              </div>
+            </div>
+            {/*End of Mix*/}
+            {/*Mix*/}
+            <div className="">
+              <div className="bg-gray-300 w-32 h-32  rounded-md shadow-md"></div>
+              <div className="">
+                <p className="text-base font-bold">Afterglow</p>
+                <p className="text-sm">DJ Lyta</p>
+              </div>
+            </div>
+            {/*End of Mix*/}
+            {/*Mix*/}
+            <div className="">
+              <div className="bg-gray-300 w-32 h-32  rounded-md shadow-md"></div>
+              <div className="">
+                <p className="text-base font-bold">Afterglow</p>
+                <p className="text-sm">DJ Lyta</p>
+              </div>
+            </div>
+            {/*End of Mix*/}
+            {/*Mix*/}
+            <div className="">
+              <div className="bg-gray-300 w-32 h-32  rounded-md shadow-md"></div>
+              <div className="">
+                <p className="text-base font-bold">Afterglow</p>
+                <p className="text-sm">DJ Lyta</p>
+              </div>
+            </div>
+            {/*End of Mix*/}
+            {/*Mix*/}
+            <div className="">
+              <div className="bg-gray-300 w-32 h-32  rounded-md shadow-md"></div>
+              <div className="">
+                <p className="text-base font-bold">Afterglow</p>
+                <p className="text-sm">DJ Lyta</p>
+              </div>
+            </div>
+            {/*End of Mix*/}
+            {/*Mix*/}
+            <div className="">
+              <div className="bg-gray-300 w-32 h-32  rounded-md shadow-md"></div>
+              <div className="">
+                <p className="text-base font-bold">Afterglow</p>
+                <p className="text-sm">DJ Lyta</p>
+              </div>
+            </div>
+            {/*End of Mix*/}
+            {/*Mix*/}
+            <div className="">
+              <div className="bg-gray-300 w-32 h-32  rounded-md shadow-md"></div>
+              <div className="">
+                <p className="text-base font-bold">Afterglow</p>
+                <p className="text-sm">DJ Lyta</p>
+              </div>
+            </div>
+            {/*End of Mix*/}
           </div>
-
-          <div className="max-w-md mx-auto mt-2 flex flex-row  ">
-            {/*Mix*/}
-            <div className="container">
-              <div className="bg-gray-300 w-32 h-32 rounded-md shadow-md"></div>
-              <p>Partyglow</p>
-              <p>Dj Shady</p>
-            </div>
-            {/*Mix*/}
-            <div className="container">
-              <div className="bg-gray-300 w-32 h-32 rounded-md shadow-md"></div>
-              <p>Anyone</p>
-              <p>Dj Dree</p>
-            </div>
-            {/*Mix*/}
-            <div className="container">
-              <div className="bg-gray-300 w-32 h-32 rounded-md shadow-md"></div>
-              <p>Cry for Me</p>
-              <p>Dj Tryce</p>
-            </div>
-          </div>
+          {/*End Grid*/}
         </div>
-        <div className="max-w-md mx-auto mt-2 flex flex-row  ">
-          {/*Mix*/}
-          <div className="container">
-            <div className="bg-gray-300 w-32 h-32 rounded-md shadow-md"></div>
-            <p>Partyglow</p>
-            <p>Dj Shady</p>
-          </div>
-          {/*Mix*/}
-          <div className="container">
-            <div className="bg-gray-300 w-32 h-32 rounded-md shadow-md"></div>
-            <p>Anyone</p>
-            <p>Dj Dree</p>
-          </div>
-          {/*Mix*/}
-          <div className="container">
-            <div className="bg-gray-300 w-32 h-32 rounded-md shadow-md"></div>
-            <p>Cry for Me</p>
-            <p>Dj Tryce</p>
-          </div>
-        </div>
+        {/*End Main Card*/}
       </div>
     </>
   );
--- src/user/MusicPage.tsx
+++ src/user/MusicPage.tsx
@@ -1,81 +1,220 @@
+import { AiOutlineHeart } from "react-icons/ai";
+import { BiDotsVerticalRounded, BiShuffle } from "react-icons/bi";
+
+import { BsFillPlayFill } from "react-icons/bs";
 const MusicPage = () => {
   return (
     <>
       <div className="container">
+        {/*Subtitles*/}
         <div className="flex justify-between">
-          <h5>My Music</h5>
-          <div className="flex ml-4">
-            <p>Play</p>
-            <p>Shuffle</p>
+          <div className="text-red-600">
+            <h5 className=" sm:text-base md:text-xl lg:2xl">My Music</h5>
+          </div>
+          <div className="flex gap-x-5 text-red-600">
+            <div className="flex">
+              <button className=" sm:text-sm md:text-md lg:text-md">
+                <BsFillPlayFill />
+              </button>
+              <button className=" sm:text-sm md:text-md lg:text-md ml-1">
+                Play
+              </button>
+            </div>
+            <div className="flex mr-2">
+              <button className="sm:text-sm md:text-md lg:text-md">
+                <BiShuffle />
+              </button>
+              <button className="sm:text-sm md:text-md lg:text-md ml-1">
+                Shuffle
+              </button>
+            </div>
           </div>
         </div>
+        {/*End of Subtitle*/}
 
-        <div className="max-w-md mx-auto mt-2 flex flex-col  ">
+        <div className="ml-5 mr-5 mx-auto mt-5 flex flex-col  ">
           {/*Card*/}
-          <div className="bg-white shadow-md rounded-lg p-2 mt-2 flex  items-center">
-            <div className="w-20 bg-gray-400 h-10 rounded-sm overflow-hidden"></div>
-            <div className="ml-4">
-              <h2 className="text-lg font-semibold">Anyone</h2>
-              <p className="text-md text-gray-600">Justin Bieber</p>
+          <div className=" container bg-white shadow-md w-full rounded-lg p-1 mt-2 flex  items-center justify-between">
+            <div className="flex">
+              <div className=" sm:w-10 md:w-14 lg:w-16 sm:h-10 md:h-14 lg:h-16 bg-gray-400  rounded-sm "></div>
+              <div className="ml-4">
+                <h4 className="sm:text-sm md:text-md lg:text-lg font-semibold">
+                  Anyone
+                </h4>
+                <h6 className="sm:text-xs md:text-sm lg:text-md text-gray-600">
+                  Justin Bieber
+                </h6>
+              </div>
             </div>
-            <div className="flex justify-between">
-              <p className="text-sm">3.12</p>
-              <button className="text-sm">likes</button>
-              <button className="text-sm">:</button>
+            <div className="flex ">
+              <button className="sm:text-xs md:text-sm lg:text-base sm:mr-5 md:mr-5 lg:mr-10">
+                3.22
+              </button>
+              <button className="sm:text-xs md:text-sm lg:text-base sm:mr-8 md:mr-8 lg:mr-10">
+                <AiOutlineHeart />
+              </button>
+              <button className=" sm:text-xs md:text-sm lg:text-base sm:mr-8 md:mr-8 lg:mr-10">
+                <BiDotsVerticalRounded />
+              </button>
             </div>
           </div>
+          {/*End of card*/}
           {/*Card*/}
-          <div className="bg-white shadow-md rounded-lg p-2 mt-2 flex  items-center">
-            <div className="w-20 bg-gray-400 h-10 rounded-sm overflow-hidden"></div>
-            <div className="ml-4">
-              <h2 className="text-lg font-semibold">Leave the door open</h2>
-              <p className="text-md text-gray-600">Bruno Mars, Silk Sonic</p>
+          <div className=" container bg-white shadow-md w-full rounded-lg p-1 mt-2 flex  items-center justify-between">
+            <div className="flex">
+              <div className=" sm:w-10 md:w-14 lg:w-16 sm:h-10 md:h-14 lg:h-16 bg-gray-400  rounded-sm "></div>
+              <div className="ml-4">
+                <h4 className="sm:text-sm md:text-md lg:text-lg font-semibold">
+                  Anyone
+                </h4>
+                <h6 className="sm:text-xs md:text-sm lg:text-md text-gray-600">
+                  Justin Bieber
+                </h6>
+              </div>
             </div>
-            <div className="flex justify-between">
-              <p className="text-sm">3.12</p>
-              <button className="text-sm">likes</button>
-              <button className="text-sm">:</button>
+            <div className="flex ">
+              <button className="sm:text-xs md:text-sm lg:text-base sm:mr-5 md:mr-5 lg:mr-10">
+                3.22
+              </button>
+              <button className="sm:text-xs md:text-sm lg:text-base sm:mr-8 md:mr-8 lg:mr-10">
+                <AiOutlineHeart />
+              </button>
+              <button className=" sm:text-xs md:text-sm lg:text-base sm:mr-8 md:mr-8 lg:mr-10">
+                <BiDotsVerticalRounded />
+              </button>
             </div>
           </div>
+          {/*End of card*/}
           {/*Card*/}
-          <div className="bg-white shadow-md rounded-lg p-2 mt-2 flex  items-center">
-            <div className="w-20 bg-gray-400 h-10 rounded-sm overflow-hidden"></div>
-            <div className="ml-4">
-              <h2 className="text-lg font-semibold">Anyone</h2>
-              <p className="text-md text-gray-600">Justin Bieber</p>
+          <div className=" container bg-white shadow-md w-full rounded-lg p-1 mt-2 flex  items-center justify-between">
+            <div className="flex">
+              <div className=" sm:w-10 md:w-14 lg:w-16 sm:h-10 md:h-14 lg:h-16 bg-gray-400  rounded-sm "></div>
+              <div className="ml-4">
+                <h4 className="sm:text-sm md:text-md lg:text-lg font-semibold">
+                  Anyone
+                </h4>
+                <h6 className="sm:text-xs md:text-sm lg:text-md text-gray-600">
+                  Justin Bieber
+                </h6>
+              </div>
             </div>
-            <div className="flex justify-between">
-              <p className="text-sm">3.12</p>
-              <button className="text-sm">likes</button>
-              <button className="text-sm">:</button>
+            <div className="flex ">
+              <button className="sm:text-xs md:text-sm lg:text-base sm:mr-5 md:mr-5 lg:mr-10">
+                3.22
+              </button>
+              <button className="sm:text-xs md:text-sm lg:text-base sm:mr-8 md:mr-8 lg:mr-10">
+                <AiOutlineHeart />
+              </button>
+              <button className=" sm:text-xs md:text-sm lg:text-base sm:mr-8 md:mr-8 lg:mr-10">
+                <BiDotsVerticalRounded />
+              </button>
             </div>
           </div>
+          {/*End of card*/}
           {/*Card*/}
-          <div className="bg-white shadow-md rounded-lg p-2 mt-2 flex  items-center">
-            <div className="w-20 bg-gray-400 h-10 rounded-sm overflow-hidden"></div>
-            <div className="ml-4">
-              <h2 className="text-lg font-semibold">Anyone</h2>
-              <p className="text-md text-gray-600">Justin Bieber</p>
+          <div className=" container bg-white shadow-md w-full rounded-lg p-1 mt-2 flex  items-center justify-between">
+            <div className="flex">
+              <div className=" sm:w-10 md:w-14 lg:w-16 sm:h-10 md:h-14 lg:h-16 bg-gray-400  rounded-sm "></div>
+              <div className="ml-4">
+                <h4 className="sm:text-sm md:text-md lg:text-lg font-semibold">
+                  Anyone
+                </h4>
+                <h6 className="sm:text-xs md:text-sm lg:text-md text-gray-600">
+                  Justin Bieber
+                </h6>
+              </div>
             </div>
-            <div className="flex justify-between">
-              <p className="text-sm">3.12</p>
-              <button className="text-sm">likes</button>
-              <button className="text-sm">:</button>
+            <div className="flex ">
+              <button className="sm:text-xs md:text-sm lg:text-base sm:mr-5 md:mr-5 lg:mr-10">
+                3.22
+              </button>
+              <button className="sm:text-xs md:text-sm lg:text-base sm:mr-8 md:mr-8 lg:mr-10">
+                <AiOutlineHeart />
+              </button>
+              <button className=" sm:text-xs md:text-sm lg:text-base sm:mr-8 md:mr-8 lg:mr-10">
+                <BiDotsVerticalRounded />
+              </button>
             </div>
           </div>
+          {/*End of card*/}
           {/*Card*/}
-          <div className="bg-white shadow-md rounded-lg p-2 mt-2 flex  items-center">
-            <div className="w-20 bg-gray-400 h-10 rounded-sm overflow-hidden"></div>
-            <div className="ml-4">
-              <h2 className="text-lg font-semibold">Gone</h2>
-              <p className="text-md text-gray-600">Rose</p>
+          <div className=" container bg-white shadow-md w-full rounded-lg p-1 mt-2 flex  items-center justify-between">
+            <div className="flex">
+              <div className=" sm:w-10 md:w-14 lg:w-16 sm:h-10 md:h-14 lg:h-16 bg-gray-400  rounded-sm "></div>
+              <div className="ml-4">
+                <h4 className="sm:text-sm md:text-md lg:text-lg font-semibold">
+                  Anyone
+                </h4>
+                <h6 className="sm:text-xs md:text-sm lg:text-md text-gray-600">
+                  Justin Bieber
+                </h6>
+              </div>
             </div>
-            <div className="flex justify-between">
-              <p className="text-sm">3.12</p>
-              <button className="text-sm">likes</button>
-              <button className="text-sm">:</button>
+            <div className="flex ">
+              <button className="sm:text-xs md:text-sm lg:text-base sm:mr-5 md:mr-5 lg:mr-10">
+                3.22
+              </button>
+              <button className="sm:text-xs md:text-sm lg:text-base sm:mr-8 md:mr-8 lg:mr-10">
+                <AiOutlineHeart />
+              </button>
+              <button className=" sm:text-xs md:text-sm lg:text-base sm:mr-8 md:mr-8 lg:mr-10">
+                <BiDotsVerticalRounded />
+              </button>
             </div>
           </div>
+          {/*End of card*/}
+          {/*Card*/}
+          <div className=" container bg-white shadow-md w-full rounded-lg p-1 mt-2 flex  items-center justify-between">
+            <div className="flex">
+              <div className=" sm:w-10 md:w-14 lg:w-16 sm:h-10 md:h-14 lg:h-16 bg-gray-400  rounded-sm "></div>
+              <div className="ml-4">
+                <h4 className="sm:text-sm md:text-md lg:text-lg font-semibold">
+                  Anyone
+                </h4>
+                <h6 className="sm:text-xs md:text-sm lg:text-md text-gray-600">
+                  Justin Bieber
+                </h6>
+              </div>
+            </div>
+            <div className="flex ">
+              <button className="sm:text-xs md:text-sm lg:text-base sm:mr-5 md:mr-5 lg:mr-10">
+                3.22
+              </button>
+              <button className="sm:text-xs md:text-sm lg:text-base sm:mr-8 md:mr-8 lg:mr-10">
+                <AiOutlineHeart />
+              </button>
+              <button className=" sm:text-xs md:text-sm lg:text-base sm:mr-8 md:mr-8 lg:mr-10">
+                <BiDotsVerticalRounded />
+              </button>
+            </div>
+          </div>
+          {/*End of card*/}
+          {/*Card*/}
+          <div className=" container bg-white shadow-md w-full rounded-lg p-1 mt-2 flex  items-center justify-between">
+            <div className="flex">
+              <div className=" sm:w-10 md:w-14 lg:w-16 sm:h-10 md:h-14 lg:h-16 bg-gray-400  rounded-sm "></div>
+              <div className="ml-4">
+                <h4 className="sm:text-sm md:text-md lg:text-lg font-semibold">
+                  Anyone
+                </h4>
+                <h6 className="sm:text-xs md:text-sm lg:text-md text-gray-600">
+                  Justin Bieber
+                </h6>
+              </div>
+            </div>
+            <div className="flex ">
+              <button className="sm:text-xs md:text-sm lg:text-base sm:mr-5 md:mr-5 lg:mr-10">
+                3.22
+              </button>
+              <button className="sm:text-xs md:text-sm lg:text-base sm:mr-8 md:mr-8 lg:mr-10">
+                <AiOutlineHeart />
+              </button>
+              <button className=" sm:text-xs md:text-sm lg:text-base sm:mr-8 md:mr-8 lg:mr-10">
+                <BiDotsVerticalRounded />
+              </button>
+            </div>
+          </div>
+          {/*End of card*/}
         </div>
       </div>
     </>
