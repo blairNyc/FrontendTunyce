ae7305308415e0c5b370fd559ef0f6c6833b87da
OliverMengich <kemeioliver2000@gmail.com>
2023-09-11 16:30:27 +0300

    feat: responsive header, layout and Sidebar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
-
+import { useState } from "react";
+// import { AiOutlineMenu } from "react-icons/ai";
+// import TunyceLogo from '/tunyce_logo.svg';
 function Layout() {
+    const [sideBarOpen, setSideBarOpen] = useState<boolean>(true)
     return (
-        <div className="container w-full h-screen">
-            <div className="w-full grid grid-cols-4">
-                <div className="col-span-1  ">
-                    <Sidebar/>
+        <div className="w-screen h-screen">
+            <div className="flex w-full">
+                <div className={`w-0 ${!sideBarOpen?'w-16':'md:w-1/5'} pt-4 `}>
+                    <Sidebar sideBarOpen={sideBarOpen}/>
                 </div>
-                <div className="bg-bg-primary col-span-3 col-start-2 col-end-4">
-                    <Header/>
+                <div className={`bg-bg-primary px-4 py-8 h-full w-full ${sideBarOpen?'md:w-4/5':'md:w-5/5'}`}>
+                    <Header sideBarOpen={sideBarOpen} setSideBarOpen={()=>{setSideBarOpen(!sideBarOpen)}}/>
                     <Outlet/>
                 </div>
             </div>
