652fe0afb7b76aaa604e01e33544ba5a4480d4b0
blairNyc <blair.milimo@tunycemedia.com>
2023-09-12 10:48:18 +0300

    feat: content creator folder

---
+++ src/creator/components/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Layout() {
+    return (
+        <div className='w-screen h-screen'>
+            <h2>This is the layout of content creator page</h2>
+        </div>
+    );
+}
+
+export default Layout;
