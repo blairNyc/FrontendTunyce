2edf4cae3ca161affc49df5f45df374582166743
OliverMengich <kemeioliver2000@gmail.com>
2023-09-15 15:35:40 +0300

    fix: removes persistor

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { store, persistor } from "./app/store";
+import { store } from "./app/store";
 import { Provider } from 'react-redux';
 
 
