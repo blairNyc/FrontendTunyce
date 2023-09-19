4898b8c534ae7f5eb532c37c6503b71e4eea6670
OliverMengich <kemeioliver2000@gmail.com>
2023-09-19 11:17:49 +0300

    fix: responsive design

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 // import { NavElement } from "./Sidebar";
 import { BsFire, BsFillPersonFill } from "react-icons/bs";
-import { FaCompass, FaDeezer } from "react-icons/fa6";
+import { FaDeezer } from "react-icons/fa6";
 import { FiSearch } from "react-icons/fi";
 import { GoHomeFill } from "react-icons/go";
 import TunyceLogo from '/tunyce_logo.svg';
@@ -19,12 +19,13 @@ function NavElement({ name, path,  children }: NavElProps) {
 interface ActionProps extends React.HTMLAttributes<HTMLButtonElement> {
     text: string
     info: string
+    button_label:string
 }
-const Actions = ({text,info}:ActionProps)=>(
-    <div className="bg-bg-primary my-2 w-4/5 px-3 rounded-md py-2">
+const Actions = ({text, button_label,info}:ActionProps)=>(
+    <div className="bg-slate-200 my-2 w-4/5 px-3 rounded-md py-2">
         <h3 className="font-bold text-sm">{text}</h3>    
-        <p>{info}</p>
-        <button className='w-4/5 py-1 px-1 uppercase hover:bg-red-600 my-3 font-bold text-white bg-text-primary'>Create Playlist</button>
+        <p className="text-xs">{info}</p>
+        <button className='w-3/5 rounded-md py-1 text-sm hover:bg-red-600 my-3 font-bold text-white bg-text-primary'>{button_label}</button>
     </div>
 )
 function DefaultLayout() {
@@ -33,7 +34,7 @@ function DefaultLayout() {
     console.log(openSideBar)
     return (
         <div className="w-screen h-screen">
-            <div className="flex w-full">
+            <div className="flex h-full w-full">
                 <div className={`w-0 ${!sideBarOpen?'w-16':'md:w-1/5'} pt-4 `}>
                 <div className='flex flex-col w-full justify-center items-center ' >
                         <img alt='tunyce logo' className='w-32 h-auto' src={TunyceLogo} />
@@ -43,30 +44,31 @@ function DefaultLayout() {
                                 <NavElement path='/'  name='Discover'>
                                     <GoHomeFill className='text-xl' />
                                 </NavElement>
-                                <NavElement path='explore' name='Explorer'>
-                                    <FaCompass className='text-xl' />
-                                </NavElement>
+                                
                                 <NavElement path='/search'  name='Search'>
                                     <FiSearch className='text-xl' />
                                 </NavElement>
                             </ul>
                             <h2 className='text-lg font-medium ml-3 mt-1'>LIBRARY</h2>
                             <ul>
-                                <NavElement name='Trending'>
+                                <NavElement path="/trending" name='Trending'>
                                     <BsFire className='text-xl' />
                                 </NavElement>
-                                <NavElement name='New'>
+                                <NavElement path="/new" name='New'>
                                     <FaDeezer className='text-xl' />
                                 </NavElement>
-                                <NavElement name='Artists' path='/artist'>
+                                <NavElement name='Artists' path='/artists'>
                                     <BsFillPersonFill className='text-xl' />
                                 </NavElement>
                             </ul>
                             <div className="flex flex-col items-center">
-                                <Actions text="Create you First playlist" info="it's easy, we'll help you"/>
-                                <Actions text="Enjoy greate podcasts" info="it's easy, we'll help you"/>
+                                <Actions button_label="Create Playlist" text="Create you First playlist" info="it's easy, we'll help you"/>
+                                <Actions button_label="Stream podcasts" text="Enjoy greate podcasts" info="it's easy, we'll help you"/>
                             </div>
-                            <button className='w-4/5 py-1 px-1 hover:bg-red-600 my-3 font-bold text-white bg-text-primary'>SIGN IN</button>
+                            <div className="flex flex-col items-center">
+                                <button className='w-3/5 py-1 px-1 text-center hover:bg-red-600 my-3 font-bold rounded-md text-white bg-text-primary'>SIGN IN</button>
+                                <p className="text-sm text-slate-600 leading-3 w-4/5">Sign in to create & share playlists, get personalized recommendations and more.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
