c279f603a377873f73fe5be97d11dc30854d32a5
OliverMengich <kemeioliver2000@gmail.com>
2023-09-23 13:29:51 +0300

    chore: usertypes type

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export interface UserTypes{
+    "is_normaluser"?:true, 
+    "is_restaunt"?:true,
+    "is_superuser"?:true,
+    "is_matatu"? : true,
+    "is_filmmaker"? : true,
+    "is_contentcreator"? : true,
+    "is_recordlabel"? : true,
+}
