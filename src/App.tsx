af55af513be31048963cc03df0e6177c82c2c223
OliverMengich <kemeioliver2000@gmail.com>
2023-09-23 13:48:27 +0300

    chore: logged in user setting

--- src/App.tsx
+++ src/App.tsx
@@ -20,101 +20,106 @@ import DefaultLayout from './components/DefaultLayout';
 import RestaurantLayout from './restaurant/components/RestaurantLayout';
 import RestaurantHomePage from './restaurant/pages/RestaurantHomePage';
 import RestaurantWalletPage from './restaurant/pages/RestaurantWalletPage';
+import { useAppSelector } from './app/hooks';
+import { RootState } from './app/store';
 const NotFoundRouter: RouteObject = {
   path: "*",
   element: <NotFound />,
 }
-type UserType = LooseAutoComplete<"is_normaluser" | "is_restaunt" >;
-type LooseAutoComplete<T extends string> = T | Omit<string, T>;
-const curr_user_type:UserType= "is_restaunt";
-const router = createBrowserRouter([
-  curr_user_type==="is_restaunt"? {
-    element: <RestaurantLayout/>,
-    children:[
-      {
-        path:'/restaurant/',
-        element: <RestaurantHomePage/>
-      },
-      {
-        path:'/restaurant/my-wallet',
-        element:<RestaurantWalletPage/>
-      },
-      NotFoundRouter,
-    ]
-  }:NotFoundRouter,
-  curr_user_type===""?{
-    element: <DefaultLayout/>,
-    children:[
-      {
-        path:'/',
-        element: <LandingPage/>
-      },
-      NotFoundRouter,
-    ]
-  }:NotFoundRouter,
-  NotFoundRouter,
-  {
-    path:'/faqs',
-    element: <FreqAskedQuesPage/>
-  },
-  {
-    path:'/terms-conditions',
-    element: <TermsConditionsPage/>
-  },
-  curr_user_type==="is_normaluser"?{
-    element: <UserLayout />,
-    children: [
-      NotFoundRouter,
-      {
-        path:'/home',
-        element: <Homepage/>
-      },
-      {
-        path:'/explore',
-        element: <ExplorePage/>
-      },
-      {
-        path:'/creators',
-        element: <CreatorsPage/>
-      },
-      {
-        path: '/creators/deejays',
-        element: <CreatorsList/>
-      },
-      {
-        path: '/creators/deejays/:id',
-        element: <Creator/>
-      },
-      {
-        path:'creators/videos/:id',
-        element: <VideoScreen/>
-      },
-      {
-        path: "/music",
-        element: <MusicPage />,
-      },
-      {
-        path: "/mixes",
-        element: <MixesPage />,
-      },
-      {
-        path: "/artist",
-        element: <ArtistPage />,
-      },
-    ]
-  }:NotFoundRouter,
-  {
-    element: <Login/>,
-    path:'/login',
-  },
-  {
-    element:<SignUp/>,
-    path:'/signup'
-  }
-])
-
-
 function App() {
+  const curr_loggedin_user= useAppSelector((state:RootState)=>state.auth.auth.curr_loggedin_user);
+  console.log(curr_loggedin_user);
+  const router = createBrowserRouter([
+    curr_loggedin_user==="is_restaunt"? {
+      element: <RestaurantLayout/>,
+      children:[
+        {
+          path:'/restaurant/',
+          element: <RestaurantHomePage/>
+        },
+        {
+          path:'/restaurant/my-wallet',
+          element:<RestaurantWalletPage/>
+        },
+        NotFoundRouter,
+      ]
+    }:NotFoundRouter,
+    curr_loggedin_user===""?{
+      element: <DefaultLayout/>,
+      children:[
+        {
+          path:'/',
+          element: <LandingPage/>
+        },
+        {
+          path: "*",
+          element: <NotFound />,
+        },
+      ]
+    }:{},
+    // NotFoundRouter,
+    {
+      path:'/faqs',
+      element: <FreqAskedQuesPage/>
+    },
+    {
+      path:'/terms-conditions',
+      element: <TermsConditionsPage/>
+    },
+    curr_loggedin_user==="is_normaluser"?{
+      element: <UserLayout />,
+      children: [
+        {
+          path: "*",
+          element: <NotFound />,
+        },
+        {
+          path:'/home',
+          element: <Homepage/>
+        },
+        {
+          path:'/explore',
+          element: <ExplorePage/>
+        },
+        {
+          path:'/creators',
+          element: <CreatorsPage/>
+        },
+        {
+          path: '/creators/deejays',
+          element: <CreatorsList/>
+        },
+        {
+          path: '/creators/deejays/:id',
+          element: <Creator/>
+        },
+        {
+          path:'creators/videos/:id',
+          element: <VideoScreen/>
+        },
+        {
+          path: "/music",
+          element: <MusicPage />,
+        },
+        {
+          path: "/mixes",
+          element: <MixesPage />,
+        },
+        {
+          path: "/artist",
+          element: <ArtistPage />,
+        },
+      ]
+    }:NotFoundRouter,
+    {
+      element: <Login/>,
+      path:'/login',
+    },
+    {
+      element:<SignUp/>,
+      path:'/signup'
+    }
+  ]);
   return <RouterProvider router={router} />;
 }
 
