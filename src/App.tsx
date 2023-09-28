9116d651467b4158adc8c268ca735e0a16017993
blairMe <felixanachoni@gmail.com>
2023-09-28 10:17:19 +0300

    importation fix

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,6 @@ import MatatuLayout from './matatus/components/MatatuLayout';
 // import { UserTypes } from './types';
 import InnerPage from './components/inner-page'
 import FilmmakerWalletPage from './components/filmMaker/FilmmakerWallet';
-import ImageUpload from './matatus/ImageUpload';
 
 
 const NotFoundRouter: RouteObject = {
@@ -302,10 +301,6 @@ function App() {
           path: '/matatu',
           element: <MatatuPage />,
         },
-        {
-          path: '/imageUpload',
-          element: <ImageUpload />
-        }
       ]
     }:curr_loggedin_user === "is_matatu" ? {
 		element:<MatatuLayout/>,
