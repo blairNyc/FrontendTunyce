6ddec00037e5f547431b29d7fecd9c143ea0a61b
blairMe <felixanachoni@gmail.com>
2023-10-01 16:58:11 +0300

    Restaurant

--- src/restaurant/components/AddRestaurantPage.tsx
+++ src/restaurant/components/AddRestaurantPage.tsx
@@ -169,7 +169,7 @@ function AddRestaurantModal({ isOpen, onClose }: { isOpen: boolean, onClose: () 
                                                     }}
                                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                 >
-                                                    <option value="" label="Select a route" />
+                                                    <option value="" label="Select a location" />
                                                     {Array.isArray(locations) && locations.map((location) => (
                                                         <option key={location.id} value={location.id} label={location.name} />
                                                     ))}
