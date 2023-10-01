174a2223cdd0ed9c5eff987780d2575a25703cb3
blairMe <felixanachoni@gmail.com>
2023-10-01 21:21:04 +0300

    restaurant title

--- src/restaurant/components/AddRestaurantPage.tsx
+++ src/restaurant/components/AddRestaurantPage.tsx
@@ -134,7 +134,7 @@ function AddRestaurantModal({ isOpen, onClose }: { isOpen: boolean, onClose: () 
 
                                 <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
                                     <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                                        New Matatu
+                                        New Restaurant
                                     </h3>
                                     <button type="button" onClick={onClose} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="staticModal"
                                     >
