9d56c93261f811902fbd56d083297a63d7c1f515
OliverMengich <kemeioliver2000@gmail.com>
2023-10-09 10:43:19 +0300

    fix: hides sidebar upon click

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ function Layout() {
     <div className="w-screen h-screen">
         <div className="flex w-full">
             <div className={`w-0  ${!sideBarOpen ? "absolute z-50 w-3/5 h-full md:hidden bg-white" : "md:w-1/5 h-full"} pt-4 `}>
-                <Sidebar toggleSideBar={toggleSideBar} />
+                <Sidebar sideBarOpen={sideBarOpen} toggleSideBar={toggleSideBar} />
             </div>
             <div className={`bg-bg-primary px-4 py-8 h-full w-full ${
                     sideBarOpen ? "md:w-4/5" : "md:w-5/5"
