a09fdd4472d5f814463dfc8883c934833fbf9dc0
dancan-sandys <dancan.oruko96@gmail.com>
2023-11-27 19:36:08 -0500

    fix controller display

--- src/components/controller/ControllerMusicPage.tsx
+++ src/components/controller/ControllerMusicPage.tsx
@@ -281,6 +281,7 @@ export default function ControllerMusicPage() {
                         </div>
                     </div>
                 </div>
+                
                 {/* <div className="m-2"> */}
                     {/* <label htmlFor="countries" className="block mb-2 text-sm font-bold text-gray-900 dark:text-white">Filter Songs by genres</label> */}
                     {/* <select id="countries" onChange={handleSelect} className="bg-gray-50 border-2 border-gray-700 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-4/5 md:w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
